210926f728a576cb00d64fed8f91600e0c4a89ad
Chinmay <chinmay.kulkarni@ariantechsolutions.com>
2025-12-26 15:25:32 +0530

    updated code on 26-12-2025

--- vide_interactive/js/apiconfig.js
+++ vide_interactive/js/apiconfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-window.API_BASE_URL = "http://103.75.226.215:8081/jsonApi";
+window.API_BASE_URL = "http://103.75.226.215:8081/api";
 // apiConfig.js
-window.API_BASE_URL_Video = "http://103.75.226.215:8080/api";
+window.API_BASE_URL_Video = "http://103.75.226.215:8081/api";
